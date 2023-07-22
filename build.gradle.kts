e8f6819c66afbd6004ddb0c1c2e4ec4c4d895783
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2023-07-22 12:00:07 +0200

    :sparkles: Feature(Build) Adding rest-assured
    In order to build clean integration tests, for testing the api of the application, rest-assured was introduced into the project.
    
    This will from now on be used for all rest based integration tests, instead of spring boot native mechanics.

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
+    testImplementation("io.rest-assured:rest-assured")
 }
 
 tasks.withType<KotlinCompile> {
